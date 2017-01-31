4067e971d37ef433008c986c812d05a26270ded6
leoniwami <leoniwamidragon@gmail.com>
2017-01-31 19:20:37 +0900

    Initial Commit

---
+++ musicshotoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicshotoku
+//
+//  Created by Leon Iwami on 2017/01/31.
+//  Copyright © 2017年 Leon Iwami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
